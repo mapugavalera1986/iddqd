b489f18dabe36f87ea0f2fdc2a4249e38417b927
mapugavalera1986 <i201924036@gmail.com>
2023-06-25 00:34:39 -0500

    Ya está empezando a tener lugar la pantalla para iniciar sesión. Todavía quedan algunos elementos que ajustar.

--- app/src/main/java/pe/edu/cibertec/iddqd/MainActivity.kt
+++ app/src/main/java/pe/edu/cibertec/iddqd/MainActivity.kt
@@ -1,28 +1,33 @@
 package pe.edu.cibertec.iddqd
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import pe.edu.cibertec.iddqd.ui.tareas.Iniciar
 import pe.edu.cibertec.iddqd.ui.theme.ReportarVideojuegosTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReportarVideojuegosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold {
+                    NavigationComponent()
                 }
             }
         }
@@ -30,17 +35,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavigationComponent(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Home"){
+        composable("Home"){
+            Iniciar(navController)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ReportarVideojuegosTheme {
-        Greeting("Android")
-    }
+fun HomeScreen(navController: NavController){
+    Text(text = "Si puedes ver esto, créeme, no todo está perdido.")
 }
---
+++ app/src/main/java/pe/edu/cibertec/iddqd/ui/tareas/Iniciar.kt
@@ -0,0 +1,85 @@
+package pe.edu.cibertec.iddqd.ui.tareas
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import pe.edu.cibertec.iddqd.data.model.Participante
+import pe.edu.cibertec.iddqd.data.remote.ApiClient
+import pe.edu.cibertec.iddqd.data.repository.ParticipanteRepository
+import pe.edu.cibertec.iddqd.ui.theme.ReportarVideojuegosTheme
+import pe.edu.cibertec.iddqd.util.Result
+
+@Composable
+fun Iniciar(navController: NavController){
+    val dni = remember{
+        mutableStateOf(TextFieldValue())
+    }
+    val context = LocalContext.current
+    val repoParticipante = ParticipanteRepository()
+    //INICIO de los elementos gráficos
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Iniciar sesión con tu DNI")
+        Spacer(modifier = Modifier.height(32.dp))
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 0.dp, 16.dp, 0.dp),
+            label = { Text(text = "DNI") },
+            value = dni.value,
+            onValueChange = {
+                dni.value = it
+            },
+            leadingIcon = { Icon(Icons.Default.Lock, null) }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier
+                .width(160.dp)
+                .padding(16.dp, 0.dp, 16.dp, 0.dp),
+            onClick = {
+
+            }
+        ) {
+            Text(text = "Ingresar")
+        }
+    }
+    //FIN de los elementos gráficos
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IniciarPreview(){
+    ReportarVideojuegosTheme {
+        Iniciar(navController = rememberNavController())
+    }
+}
